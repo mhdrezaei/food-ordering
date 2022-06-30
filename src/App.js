b5b71ccf0af7f02aa6eddfb1ada73af2990cebd2
رضائی - محمد <rezaee-moha@tehran.iri>
2022-06-30 23:00:34 +0430

    install react-cookies package

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
 import { BrowserRouter as Router , Route , Routes } from 'react-router-dom';
 import './App.css';
 import 'bootstrap';
+import './sass/main.scss'
 import {ToastContainer} from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css';
 import 'bootstrap/dist/css/bootstrap.css'
 import Header from './components/Header/Header';
-import Food from './components/Food/Food';
-import Sushi from './assets/images/food/list/sushi.jpg'
+import {FoodProvider} from './context/foodContext'
 import AddFood from './pages/AddFood';
 import Home from './pages/Home';
 import Footer from './components/Footer/Footer';
+import Cart from './components/Cart/Cart';
+import {CookiesProvider } from 'react-cookie';
 
 function App() {
 
   return (
+    <FoodProvider>
+      <CookiesProvider>
     <>
     <Router>
+      <Cart/>
     <Header />
       <Routes>
         <Route path='/' element={<Home/>} />
@@ -27,6 +32,8 @@ function App() {
     
     
     </>
+    </CookiesProvider>
+    </FoodProvider>
   );
 }
 
